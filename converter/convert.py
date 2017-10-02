bab4d9e6fcbda9fc3fb7975df8c01fa24a7cf63c
Dieter De Paepe <dieter.depaepe@gmail.com>
2017-10-02 15:32:42 +0200

    Draft spec Besluit & Mandaat 2017-10-02.

--- converter/convert.py
+++ converter/convert.py
@@ -9,9 +9,27 @@ models['besluit'] = {
     'stakeholders_csv_column': 'Besluit',
     'source_voc': 'besluit.ttl',
     'source_ap': 'Besluit Publicatie AP.tsv',
-    'template_voc': 'vocabularynlv2',
-    'template_ap': '',
-    'target_ap': 'besluit'
+    'template_voc': 'vocabularynlv2.j2',
+	'title_ap': 'Besluit Publicatie AP',
+    'target_ap': 'besluit-publicatie'
+}
+models['mandaat'] = {
+    'stakeholders_csv': 'stakeholders_lblod.csv',
+    'stakeholders_csv_column': 'Mandaat',
+    'source_voc': 'mandaat.ttl',
+    'source_ap': 'Mandatendatabank AP.tsv',
+    'template_voc': 'vocabularynlv2.j2',
+	'title_ap': 'Mandatendatabank AP',
+    'target_ap': 'mandatendatabank'
+}
+models['dienst'] = {
+    'stakeholders_csv': 'stakeholders_latest.csv',
+    'stakeholders_csv_column': 'Dienst',
+    'source_voc': 'dienst.ttl',
+    'source_ap': 'Dienstencataloog AP.tsv',
+    'template_voc': 'vocabularynlv2.j2',
+	'title_ap': 'Dienstencataloog AP',
+    'target_ap': 'dienstencataloog'
 }
 
 if len(sys.argv) == 1 or sys.argv[1] not in models.keys():
@@ -21,7 +39,7 @@ if len(sys.argv) == 1 or sys.argv[1] not in models.keys():
     print('With name one of: ', valid_names)
     exit(0)
 
-script = './specgen/OSLO-SpecificationGenerator/bin/generate_vocabulary.py'
+script = './specgen/OSLO-SpecificationGenerator/specgen/generate_vocabulary.py'
 if not os.path.isfile(script):
     print('This file expects the OSLO-SpecificationGenerator to be installed in this directory.')
     print('Could not find file: ' + script)
@@ -34,15 +52,17 @@ model = models[model_name]
 
 
 # Convert CSV of contributors to RDF for merging
+print('Converting CSV of contributors to RDF for merging.')
 stakeholders_rdf = './../src/temp-%s-%s.rdf' % (model['stakeholders_csv'], model_name)
 subprocess.run(['python', script,
                 '--contributors',
                 '--csv', './../src/' + model['stakeholders_csv'],
-                '--target', model['stakeholders_csv_column'],
+                '--csv_contributor_role_column', model['stakeholders_csv_column'],
                 '--output', stakeholders_rdf
                 ])
 
-# Merge stakeholders RDF with vocabulary TTL
+# Merge contributors RDF with vocabulary TTL
+print('Merging contributors RDF with vocabulary RDF.')
 subprocess.run(['python', script,
                 '--merge',
                 '--rdf', './../src/' + model['source_voc'],
@@ -51,6 +71,7 @@ subprocess.run(['python', script,
                 ])
 
 # Generate documentation for vocabulary
+print('Generating vocabulary documentation.')
 subprocess.run(['python', script,
                 '--rdf', './../ns/' + model['source_voc'],
 				'--schema', model['template_voc'],
@@ -58,9 +79,14 @@ subprocess.run(['python', script,
                 ])
 
 # Generate documentation for AP
+print('Generating AP documentation.')
 subprocess.run(['python', script,
                 '--ap',
+				'--title', model['title_ap'],
                 '--csv', './../src/' + model['source_ap'],
                 '--csv_contributor', './../src/' + model['stakeholders_csv'],
+				'--csv_contributor_role_column', model['stakeholders_csv_column'],
                 '--output', './../doc/ap/%s/index.html' % model['target_ap']
                 ])
+
+print('Done!')
