4675540d9e0672c8ce95a7c60d378267a9663b59
Jens Scheerlinck <jens@jesc.be>
2018-12-03 17:17:51 +0100

    Bugfixes OSLO Openbaar Domein na deployment toolchain op productie

---
+++ site-skeleton/cms/ap/objectcatalogus-terreindeel/boom.js
@@ -0,0 +1,162 @@
+           var treeData = [
+              {
+                "name": "Terreindeel",
+                "parent": "null",
+                "children": [
+                  {
+                      "name": "Wegbaan",
+                      "parent": "Terreindeel",
+                      "children": [
+                        {
+                            "name": "Rijbaan",
+                            "parent": "Wegbaan"
+                        }, 
+                        {
+                            "name": "Wegberm",
+                            "parent": "Wegbaan",
+                            "children": [
+                              {
+                                  "name": "Zijberm",
+                                  "parent": "Wegberm"
+                              },  
+                              {
+                                  "name": "Tussenberm",
+                                  "parent": "Wegberm"
+                              },  
+                              {
+                                  "name": "Middenberm",
+                                  "parent": "Wegberm"
+                              }                             
+                            ]
+                        }, 
+                        {
+                            "name": "Eigen bedding",
+                            "parent": "Wegbaan"
+                        }, 
+                        {
+                            "name": "Bijzondere overrijdbare bedding",
+                            "parent": "Wegbaan"
+                        }, 
+                        {
+                            "name": "Trottoir",
+                            "parent": "Wegbaan"
+                        }, 
+                        {
+                            "name": "Fietspad",
+                            "parent": "Wegbaan"
+                        }, 
+                        {
+                            "name": "Bushaltehaven",
+                            "parent": "Wegbaan"
+                        }, 
+                        {
+                            "name": "Straatgoot",
+                            "parent": "Wegbaan"
+                        }, 
+                        {
+                            "name": "Boordsteen",
+                            "parent": "Wegbaan"
+                        }                      
+                      ]
+                  },
+                  {
+                      "name": "Parkeervlak",
+                      "parent": "Terreindeel"
+                  },
+                  {
+                      "name": "Oprit",
+                      "parent": "Terreindeel"
+                  },
+                  {
+                      "name": "Spoorwegberm",
+                      "parent": "Terreindeel"
+                  },
+                  {
+                      "name": "Voetweg",
+                      "parent": "Terreindeel"
+                  },
+                  {
+                      "name": "Hondentoilet",
+                      "parent": "Terreindeel"
+                  },
+                  {
+                      "name": "Hondenloopzone",
+                      "parent": "Terreindeel"
+                  },
+                  {
+                      "name": "Horecaterrein",
+                      "parent": "Terreindeel"
+                  },
+                  {
+                      "name": "Receatieve ruimte",
+                      "parent": "Terreindeel",
+                      "children": [
+                        {
+                            "name": "Ligweide",
+                            "parent": "Receatieve ruimte"
+                        },
+                        {
+                            "name": "Speelruimte",
+                            "parent": "Receatieve ruimte"
+                        },
+                        {
+                            "name": "Speelterrein",
+                            "parent": "Receatieve ruimte"
+                        },
+                        {
+                            "name": "Sportterrein",
+                            "parent": "Receatieve ruimte"
+                        }
+                      ]
+                  },
+                  {
+                      "name": "Trap",
+                      "parent": "Terreindeel"
+                  },
+                  {
+                      "name": "Groenvlak",
+                      "parent": "Terreindeel"
+                  },
+                  {
+                      "name": "Moestuin",
+                      "parent": "Terreindeel"
+                  },
+                  {
+                      "name": "Hoogstamboomgaard",
+                      "parent": "Terreindeel"
+                  },
+                  {
+                      "name": "Laagstamboomgaard",
+                      "parent": "Terreindeel"
+                  },
+                  {
+                      "name": "Dierenloopweide",
+                      "parent": "Terreindeel"
+                  },
+                  {
+                      "name": "Groendak",
+                      "parent": "Terreindeel"
+                  },
+                  {
+                      "name": "Akker",
+                      "parent": "Terreindeel"
+                  },
+                  {
+                      "name": "Tijdelijk groen",
+                      "parent": "Terreindeel"
+                  },
+                  {
+                      "name": "Tuinbouwgrond",
+                      "parent": "Terreindeel"
+                  },
+                  {
+                      "name": "Oever",
+                      "parent": "Terreindeel"
+                  },
+                  {
+                      "name": "Boomplantplaats",
+                      "parent": "Terreindeel"
+                  }
+                ]
+              }
+            ];
---
+++ site-skeleton/cms/ap/objectcatalogus-vegetatie-elementen/boom.js
@@ -0,0 +1,84 @@
+ var treeData = [
+              {
+                "name": "Vegetatie Element",
+                "parent": "null",
+                "children": [
+                  {
+                    "name": "Plantbakvorm",
+                    "parent": "Vegetatie Element",
+                    "children": [
+                      {
+                        "name": "Plantbak",
+                        "parent": "Plantbakvorm"       
+                      },
+                      {
+                        "name": "Bloemenmand",
+                        "parent": "Plantbakvorm"           
+                      },
+                      {
+                        "name": "Bloementoren",
+                        "parent": "Plantbakvorm"            
+                      },
+                      {
+                        "name": "Vegetatiewand",
+                        "parent": "Plantbakvorm"            
+                      }
+                    ]
+                  },
+                  {
+                    "name": "Boomvorm",
+                    "parent": "Vegetatie Element",
+                    "children": [
+                      {
+                        "name": "Opgaande boom",
+                        "parent": "Boomvorm"          
+                      },
+                      {
+                        "name": "Vormboom",
+                        "parent": "Boomvorm",
+                        "children": [
+                          {
+                            "name": "Gekandelaarde boom",
+                            "parent": "Vormboom"
+                          }, 
+                          {
+                            "name": "Geschoren boom",
+                            "parent": "Vormboom"
+                          }, 
+                          {
+                            "name": "Knotboom",
+                            "parent": "Vormboom"
+                          }, 
+                          {
+                            "name": "Leiboom",
+                            "parent": "Vormboom"
+                          } 
+                        ]           
+                      }
+                    ]
+                  },
+                  {
+                    "name": "Klimvorm",
+                    "parent": "Vegetatie Element",
+                    "children": [
+                      {
+                        "name": "Zelfhechtende klimvorm",
+                        "parent": "Klimvorm"
+                      }, 
+                      {
+                        "name": "Niet-zelfhechtende klimvorm",
+                        "parent": "Klimvorm"
+                      }               
+                    ] 
+                  },
+                  {
+                    "name": "Muurvegetatie",
+                    "parent": "Vegetatie Element"
+                  },
+                  {
+                    "name": "Solitaire heester",
+                    "parent": "Vegetatie Element"
+                  }
+                ]
+              }
+            ];
