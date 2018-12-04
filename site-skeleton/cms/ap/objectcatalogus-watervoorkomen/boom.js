4296bc6d5300c8d0cfbaf153d6c7fd4b330ad023
Jens Scheerlinck <jens.scheerlinck@pwc.com>
2018-12-04 10:01:08 +0100

    Bugfixes OSLO Openbaar Domein na deployment toolchain op productie (#194)
    * Bugfixes OSLO Openbaar Domein na deployment toolchain op productie
    
    * Bugfix templates Openbaar Domein

---
+++ site-skeleton/cms/ap/objectcatalogus-watervoorkomen/boom.js
@@ -0,0 +1,20 @@
+            var treeData = [
+              {
+                "name": "Watervoorkomen",
+                "parent": "null",
+                "children": [
+                  {
+                    "name": "Diep water",
+                    "parent": "Watervoorkomen"
+                  },
+                  {
+                    "name": "Diep water met oevervegetatie",
+                    "parent": "Watervoorkomen"
+                  },
+                  {
+                    "name": "Ondiep water",
+                    "parent": "Watervoorkomen"
+                  }
+                ]
+              }
+            ];
