c0ade35d366167b5aa22a1c765cdfa607e15263f
Paul Massey <paul.massey@tenforce.com>
2018-12-05 14:37:56 +0000

    expanded the scripts

--- scripts/expand-links.sh
+++ scripts/expand-links.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo "expand links"
+if [ -f "/tmp/workspace/links.txt" ]
+then
+    cat /tmp/workspace/links.txt | while read line
+    do
+	echo "expand: $line"
+    done
+fi
--- scripts/extract-what.sh
+++ scripts/extract-what.sh
@@ -9,36 +9,38 @@ CHECKOUTFILE=${TARGETDIR}/checkouts.txt
 #############################################################################################
 # extraction command functions
 
-extract_tsv() {
-    local TDIR=${TARGETDIR}/tsv
-    mkdir -p ${TDIR}
-    # Extract ttl data for each diagram
+mappings_file() {
     local MAPPINGFILE="config/eap-mapping.json"
     if [ -f ".names.txt" ]
     then
-	echo "name: $(cat .names.txt)"
 	STR=".[] | select(.name | contains(\"$(cat .names.txt)\")) | [.]"
 	jq "${STR}" ${MAPPINGFILE} > .names.json
 	MAPPINGFILE=".names.json"
     fi
+    echo ${MAPPINGFILE}
+}
+
+extract_tsv() {
+    local MAPPINGFILE=$1
+    local TDIR=${TARGETDIR}/tsv
+    mkdir -p ${TDIR}
     # Extract tsv data for each diagram    
     jq -r '.[] | select(.type | contains("ap")) | @sh "java -jar /app/ea-2-rdf.jar tsv -i \(.eap) -c config/config-ap.json -d \(.diagram) -o /tmp/workspace/tsv/\(if .prefix then .prefix + "/" else "" end)\(.name).tsv"' < $MAPPINGFILE | bash
 }
 
 extract_ttl() {
+    local MAPPINGFILE=$1
     local TDIR=${TARGETDIR}/ttl
     mkdir -p ${TDIR}
-    
     # Extract ttl data for each diagram
-    local MAPPINGFILE="config/eap-mapping.json"
-    if [ -f ".names.txt" ]
-    then
-	echo "name: $(cat .names.txt)"
-	STR=".[] | select(.name | contains(\"$(cat .names.txt)\")) | [.]"
-	jq "${STR}" ${MAPPINGFILE} > .names.json
-	MAPPINGFILE=".names.json"
-    fi
     jq -r '.[] | select(.type | contains("voc")) | @sh "java -jar /app/ea-2-rdf.jar convert -i \(.eap) -c config/config-voc.json -d \(.diagram) -o /tmp/workspace/ttl/\(if .prefix then .prefix + "/" else "" end)\(.name).ttl"' $MAPPINGFILE | bash
+}
+
+extract_stakeholder() {
+    local MAPPINGFILE=$1
+    local TDIR=${TARGETDIR}/ttl
+    mkdir -p ${TDIR}
+    jq -r '.[] | select(.type | contains("voc")) | @sh "python /app/specgen/generate_vocabulary.py --add_contributors --rdf /tmp/workspace/ttl/\(if .prefix then .prefix + "/" else "" end)\(.name).ttl --csv src/stakeholders.csv --csv_contributor_role_column \(.contributors) --output /tmp/workspace/ttl/\(if .prefix then .prefix + "/" else "" end)\(.name).ttl"' < $MAPPINGFILE | bash
 }
 
 # do the conversions
@@ -55,12 +57,15 @@ do
     if [ -d "$line" ]
     then
       pushd $line
-        case $extractwhat in
-     	    tsv) extract_tsv
-		 ;;
-     	    ttl) extract_ttl
-		 ;;
-              *) echo "towhat not defined"
+       local MAPPINGFILE=$(mapping_file)    
+       case $extractwhat in
+     	         tsv) extract_tsv $MAPPINGFILE
+		      ;;
+                 ttl) extract_ttl $MAPPINGFILE
+		      ;;
+        stakeholders) extract_stakeholder $MAPPINGFILE
+		      ;;
+                   *) echo "ERROR: $extractwhat not defined"
         esac 	   
       popd
     else
