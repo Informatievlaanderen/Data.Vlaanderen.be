0853e618655a5275932f982738c82ac99f9d0a46
Paul Massey <paul.massey@tenforce.com>
2018-12-05 09:16:49 +0000

    minor(17)

--- scripts/extract-what.sh
+++ scripts/extract-what.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-# set -x
+set -x
 
 extractwhat=$1
-CHECKOUTFILE=/tmp/workspace/checkouts.txt
+TARGETDIR=${TARGETDIR}
+CHECKOUTFILE=${TARGETDIR}/checkouts.txt
 
-# extraction commands
+#############################################################################################
+# extraction command functions
 
 extract_tsv() {
+    local TDIR=${TARGETDIR}/tsv
+    mkdir -p ${TDIR}
     # Extract tsv data for each diagram    
-    jq -r '.[] | select(.type | contains("ap")) | @sh "java -jar /app/ea-2-rdf.jar tsv -i \(.eap) -c config/config-ap.json -d \(.diagram) -o /tmp/workspace/tsv/\(if .prefix then .prefix + "/" else "" end)\(.name).tsv"' < config/eap-mapping.json | bash
+    jq -r '.[] | select(.type | contains("ap")) | @sh "java -jar /app/ea-2-rdf.jar tsv -i \(.eap) -c config/config-ap.json -d \(.diagram) -o ${TDIR}/\(if .prefix then .prefix + "/" else "" end)\(.name).tsv"' < config/eap-mapping.json | bash
 }
 
 extract_ttl() {
+    local TDIR=${TARGETDIR}/ttl
+    mkdir -p ${TDIR}
     # Extract ttl data for each diagram
     local MAPPINGFILE="config/eap-mapping.json"
     if [ -f ".names.txt" ]
@@ -22,7 +28,7 @@ extract_ttl() {
 	jq "${STR}" ${MAPPINGFILE} > .names.json
 	MAPPINGFILE=".names.json"
     fi
-    jq -r '.[] | select(.type | contains("voc")) | @sh "java -jar /app/ea-2-rdf.jar convert -i \(.eap) -c config/config-voc.json -d \(.diagram) -o /tmp/workspace/ttl/\(if .prefix then .prefix + "/" else "" end)\(.name).ttl"' $MAPPINGFILE | bash
+    jq -r '.[] | select(.type | contains("voc")) | @sh "java -jar /app/ea-2-rdf.jar convert -i \(.eap) -c config/config-voc.json -d \(.diagram) -o ${TDIR}/\(if .prefix then .prefix + "/" else "" end)\(.name).ttl"' $MAPPINGFILE | bash
 }
 
 # do the conversions
@@ -35,7 +41,7 @@ fi
 
 cat ${CHECKOUTFILE} | while read line
 do
-    echo "Processing line: $line"
+    echo "Processing line ($extractwhat): $line"
     if [ -d "$line" ]
     then
       pushd $line
