93dd866a62a7154cb058f74f10b208de0853522d
Paul Massey <paul.massey@tenforce.com>
2018-12-06 10:03:45 +0000

    Updated

--- scripts/extract-what.sh
+++ scripts/extract-what.sh
@@ -24,6 +24,7 @@ extract_tsv() {
     local MAPPINGFILE=$1
     local TDIR=${TARGETDIR}/tsv
     mkdir -p ${TDIR}
+    
     # Extract tsv data for each diagram    
     jq -r '.[] | select(.type | contains("ap")) | @sh "java -jar /app/ea-2-rdf.jar tsv -i \(.eap) -c config/config-ap.json -d \(.diagram) -o /tmp/workspace/tsv/\(if .prefix then .prefix + "/" else "" end)\(.name).tsv"' < $MAPPINGFILE | bash -e
 }
@@ -41,6 +42,15 @@ extract_stakeholder() {
     local TDIR=${TARGETDIR}/ttl
     mkdir -p ${TDIR}
     jq -r '.[] | select(.type | contains("voc")) | @sh "python /app/specgen/generate_vocabulary.py --add_contributors --rdf /tmp/workspace/ttl/\(if .prefix then .prefix + "/" else "" end)\(.name).ttl --csv src/stakeholders.csv --csv_contributor_role_column \(.contributors) --output /tmp/workspace/ttl/\(if .prefix then .prefix + "/" else "" end)\(.name).ttl"' < $MAPPINGFILE | bash -e
+}
+
+extract_json() {
+    local MAPPINGFILE=$1
+    local TDIR=${TARGETDIR}/tsv
+    mkdir -p ${TDIR}
+    
+    # Extract tsv data for each diagram    
+    jq -r '.[] | select(.type | contains("ap")) | @sh "java -jar /app/ea-2-rdf.jar tsv -i \(.eap) -c config/config-ap.json -d \(.diagram) -o /tmp/workspace/tsv/\(if .prefix then .prefix + "/" else "" end)\(.name).tsv"' < $MAPPINGFILE | bash -e
 }
 
 # do the conversions
@@ -62,6 +72,8 @@ do
      	         tsv) extract_tsv $MAPPINGFILE
 		      ;;
                  ttl) extract_ttl $MAPPINGFILE
+		      ;;
+                json) extract_json $MAPPINGFILE
 		      ;;
         stakeholders) extract_stakeholder $MAPPINGFILE
 		      ;;
