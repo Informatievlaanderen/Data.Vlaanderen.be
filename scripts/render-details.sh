1b30eaff1b3d66a3104c8075fa642ca8a00a6128
Paul Massey <paul.massey@tenforce.com>
2018-12-07 08:48:20 +0000

    updated.

--- scripts/render-details.sh
+++ scripts/render-details.sh
@@ -2,5 +2,13 @@
 
 WORKDIR=$1
 SUBDIR=$2
-echo "render-details: $1 $2"
+TEMPLATE=unknown
 
+echo "render-details: starting with $1 $2 $3"
+
+for $i in ${WORkDIR}/${SUBDIR}/*.jsonld
+do
+    OUTFILE=$(basename "$i" .json).html
+    echo "render-details: convert $i to $OUTPUT"
+    echo "node cls.js $i ${TEMPLATE} ${OUTPUT}"
+done
