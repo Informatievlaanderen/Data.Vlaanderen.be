95e2d687bf2e1afcdf5fb9266835c2b0b7800c0b
Paul Massey <paul.massey@tenforce.com>
2018-12-07 09:23:30 +0000

    minor(32)

--- scripts/render-details.sh
+++ scripts/render-details.sh
@@ -6,10 +6,12 @@ TEMPLATE=unknown
 
 echo "render-details: starting with $1 $2 $3"
 
+mkdir -p ${WORKDIR}/html
+
 for i in ${WORKDIR}/${SUBDIR}/*.jsonld
 do
     echo "render-details: convert $i to html"
     OUTFILE=$(basename $i .jsonld).html
-    echo "node cls.js $i ${TEMPLATE} ${OUTFILE}"
+    echo "node cls.js $i ${TEMPLATE} ${WORKDIR}/html/${OUTFILE}"
 done
 
