b844991ed1304f22e373c949e6cde349e3685dd3
Paul Massey <paul.massey@tenforce.com>
2018-12-10 08:32:08 +0000

    template handling

--- scripts/render-details.sh
+++ scripts/render-details.sh
@@ -6,20 +6,24 @@ CHECKOUTFILE=${TARGETDIR}/checkouts.txt
 
 echo "render-details: starting with $1 $2 $3"
 
+set -x
+
 mkdir -p ${TARGETDIR}/html
 
 cat ${CHECKOUTFILE} | while read line
 do
-    echo "Processing line ($extractwhat): $line"
+    echo "Processing line: $line"
     if [ -d "$line" ]
     then
 	pushd $line
 	for i in *.jsonld
 	do
 	    echo "render-details: convert $i to html"
-	    OUTFILE=$(basename $i .jsonld).html
-	    TEMPLATE=$(basename $i .jsonld).j2
-	    node cls.js $i ${TEMPLATE} ${TARGETDIR}/html/${OUTFILE}
+	    BASENAME=$(basename $i .jsonld)
+	    OUTFILE=${BASENAME}.html
+	    TYPE=$(jq -r '.[]|select(.name | contains("${BASENAME}"))|.type' .names.json)
+	    TEMPLATE=${BASENAME}.j2
+	    node cls.js $i templates/${BASENAME}-${TYPE}.j2 ${TARGETDIR}/html/${OUTFILE}
 	done
 	popd
     else
