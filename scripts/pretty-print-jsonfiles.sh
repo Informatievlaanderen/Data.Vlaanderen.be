930e1ab09e0b5d684257d0ec6d8612a97b5b3d79
Paul Massey <paul.massey@tenforce.com>
2018-12-12 17:19:55 +0000

    commented out pretty printing - bug in code.

--- scripts/pretty-print-jsonfiles.sh
+++ scripts/pretty-print-jsonfiles.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 WORKDIR=$1
-SRCDIR=${WORKDIR}/$1
+SRCDIR=${WORKDIR}/src
 JSONDIR=${WORKDIR}/report/json
 PID=$$
 
-( find ${SRCDIR} -name \*.jsonld -type f
-; find ${JSONDIR} -name \*.jsonld -type f ) > /tmp/files.txt
+( find ${SRCDIR} -name \*.jsonld -type f ; find ${JSONDIR} -name \*.jsonld -type f ) > /tmp/files.txt
 
 pushd /app/pretty-print
  cat /tmp/files.txt | while read line
  do
-     node pretty-print.js --input $line --output $line.out --descending "foaf:last_name" --descending "foaf:first_name"
-     mv $line.out $line
+     echo "not yet working"
+     echo node pretty-print.js --input $line --output $line.out "foaf:last_name" "foaf:first_name"     
+     echo node pretty-print.js --input $line --output $line.out "foaf:last_name" "foaf:first_name"
+     echo mv $line.out $line
  done
 popd
 
