ebd5ae6949d0b4d1b481654f145e843af9e826b9
Paul Massey <paul.massey@tenforce.com>
2018-12-12 13:23:02 +0000

    src/target separation

--- scripts/extract-what.sh
+++ scripts/extract-what.sh
@@ -56,7 +56,7 @@ extract_json() {
     local RDIR=${TARGETDIR}/report
     mkdir -p ${TDIR} ${RDIR} ${TARGETDIR}/target/${LINE}
     java -jar /app/ea-2-rdf.jar jsonld -c ${MAPPINGFILE} -n $(cat .names.txt)
-    cp $(cat .names.txt).jsonld ${TARGETDIR}/target/${LINE}
+    cp $(cat .names.txt).jsonld ${TDIR}
     cp $(cat .names.txt).report ${RDIR}
     ( echo $PWD ; cat $(cat .names.txt).report ) >> ${TDIR}/ALL.report
 }
