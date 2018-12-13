cb2856a33547ffbb9f79ae3cdd9cf6c8afb5b3a2
Paul Massey <paul.massey@tenforce.com>
2018-12-13 09:08:19 +0000

    minor (1)

--- scripts/pretty-print-jsonfiles.sh
+++ scripts/pretty-print-jsonfiles.sh
@@ -10,8 +10,8 @@ PID=$$
 pushd /app/pretty-print
  cat /tmp/files.txt | while read line
  do
-     echo node pretty-print.js --input $line --output $line.out"     
-     if node pretty-print.js --input $line --output $line.out"
+     echo "node pretty-print.js --input $line --output $line.out"    
+     if node pretty-print.js --input $line --output $line.out
      then
 	 mv $line.out $line
      else
