0b17a6e5b5d90e83585108798fafc21405079c7a
Paul Massey <paul.massey@tenforce.com>
2018-12-13 11:49:53 +0000

    filename updates.

--- scripts/render-details.sh
+++ scripts/render-details.sh
@@ -52,7 +52,7 @@ render_shacl() {
     local TLINE=$2
     local JSONI=$3
     BASENAME=$(basename ${JSONI} .jsonld)
-    OUTFILE=${BASENAME}.shacl.ttl
+    OUTFILE=${BASENAME}-SHACL.ttl
     echo "node /app/shacl-generator.js -i ${JSONI} -o ${TLINE}/shacl/${OUTFILE}"
     pushd /app
       mkdir -p ${TLINE}/shacl
