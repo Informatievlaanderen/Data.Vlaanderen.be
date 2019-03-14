abda052672b26b4d5cf4498e8d99672f173f01cf
Bert Van Nuffelen <bert.van.nuffelen@tenforce.com>
2019-03-14 13:23:05 +0000

    add more data to vocabulary

--- scripts/generate-voc.sh
+++ scripts/generate-voc.sh
@@ -21,9 +21,9 @@ make_jsonld() {
 
     jq -S '.classes| map({"name" : .name, "description" : .description , "usage" : .usage, "@id" : ."@id", "@type" : ."@type", "parents" : .parents? }) |sort_by(."@id")' /tmp/${FILE}3.jsonld > /tmp/${FILE}/classes
     jq -S '.externals| map({"name" : .name,  "@id" : ."@id", "@type" : "rdfs:Class" } ) |sort_by(."@id")' /tmp/${FILE}3.jsonld > /tmp/${FILE}/externalclasses
-    jq -S '.properties| map({"name" : .name, "description" : .description , "usage" : .usage, "@id" : ."@id", "@type" : ."@type", "domain" : .domain, "range" : .range } )| sort_by(."@id")' /tmp/${FILE}3.jsonld > /tmp/${FILE}/properties
+    jq -S '.properties| map({"name" : .name, "description" : .description , "usage" : .usage, "@id" : ."@id", "@type" : ."@type", "domain" : .domain, "range" : .range, "generalization": .generalization? } )| sort_by(."@id")' /tmp/${FILE}3.jsonld > /tmp/${FILE}/properties
     jq -S '.externalproperties| map({"name" : .name,  "@id" : ."@id", "@type" : "rdf:Property" } ) | sort_by(."@id")' /tmp/${FILE}3.jsonld > /tmp/${FILE}/externalproperties
-    jq -S '{"@id" : ."@id", "@type" : ."@type", "label": .label, "title": .title?, "namespace": .namespace?, "authors" : .authors, "editors" : .editors, "contributors" : .contributors, "publication-state" : ."publication-state"?, "publication-date" : ."publication-date"?, "navigation" : .navigation?}' /tmp/${FILE}3.jsonld > /tmp/${FILE}/ontology
+    jq -S '{"@id" : ."@id", "@type" : ."@type", "label": .label, "title": .title?, "namespace": .namespace?, "authors" : .authors, "editors" : .editors, "contributors" : .contributors, "publication-state" : ."publication-state"?, "publication-date" : ."publication-date"?, "navigation" : .navigation?, "licence": .licence?}' /tmp/${FILE}3.jsonld > /tmp/${FILE}/ontology
 
     jq -s '.[0] + .[1] + {"classes": .[2], "properties": .[4], "externals": .[3], "externalproperties": .[5]} + .[6]' /tmp/${FILE}/ontology ${CONFIGDIR}/ontology.defaults.json /tmp/${FILE}/classes /tmp/${FILE}/externalclasses /tmp/${FILE}/properties /tmp/${FILE}/externalproperties ${CONFIGDIR}/context >  ${TARGET}
 }
