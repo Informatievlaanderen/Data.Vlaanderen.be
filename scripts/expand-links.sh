0941b0a9147a466dfe4a39392f59b8add020b9a3
Bert Van Nuffelen <bert.van.nuffelen@tenforce.com>
2019-06-18 07:52:56 +0000

    support prefix in the expansion of the links

--- scripts/expand-links.sh
+++ scripts/expand-links.sh
@@ -13,7 +13,7 @@ then
    jq  --arg src ${TARGET} --arg tgt ${TARGET}/context -r '.[] |if ( .urlref | startswith("/doc/applicatieprofiel") ) then  @sh "cp \($src)\(.seealso)/context/* \($tgt)" else empty end'  $LINKS | bash -e
    jq  --arg src ${TARGET} --arg tgt ${TARGET}/shacl -r '.[] | if ( .urlref | startswith("/doc/applicatieprofiel") ) then @sh "cp \($src)\(.seealso)/shacl/* \($tgt)" else empty end'  $LINKS | bash -e
 #   jq  --arg src ${TARGET} --arg tgt ${TARGET} -r '.[] | if ( .urlref | startswith("/ns") ) then @text "cp \($src)\(.seealso)/html/* \($tgt)\(.urlref)" else empty end'  $LINKS | bash -e 
-   jq  --arg src ${TARGET} --arg tgt ${TARGET}/ns -r '.[] | if ( .urlref | startswith("/ns") ) then @sh "cp \($src)\(.seealso)/voc/* \($tgt)" else empty end'  $LINKS | bash -e 
+   jq  --arg src ${TARGET} --arg tgt ${TARGET}/ns -r '.[] | if ( .urlref | startswith("/ns") ) then @sh "cp \($src)\(.seealso)/voc/* \($tgt)\(.prefix)" else empty end'  $LINKS | bash -e 
 
 
 
