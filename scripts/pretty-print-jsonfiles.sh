39a769efb71c0e6bb734d619bb0f1e95570e050a
Bert Van Nuffelen <bert.vannuffelen@gmail.com>
2021-09-09 12:21:02 +0000

    sort properties too

--- scripts/pretty-print-jsonfiles.sh
+++ scripts/pretty-print-jsonfiles.sh
@@ -11,7 +11,12 @@ pushd /app
  cat /tmp/files.txt | while read line
  do
      echo "node pretty-print.js --input $line --output $line.out"
-     if ! node pretty-print.js --input $line --output $line.out
+     if ! node pretty-print.js --input $line --output $line.out1
+     then
+	 echo "pretty print failed"
+	 exit 1
+     fi
+     if ! node pretty-print.js --input $line.out1 --output $line.out -s properties externalproperties -a '@id' 'name' 'definition' 
      then
 	 echo "pretty print failed"
 	 exit 1
