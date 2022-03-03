04e381c235637984096af9a5139df33082337d22
Joeri Moreno <morenojoeri@hotmail.com>
2022-03-03 20:35:53 +0100

    fix calculation of changed publications

--- scripts/findPublicationsToUpdate.sh
+++ scripts/findPublicationsToUpdate.sh
@@ -20,34 +20,28 @@ TOOLCHAINCONFIG=${CONFIG_FOLDER}/config.json
 rm -rf $ROOT_DIR/*.txt
 rm -rf tmp
 
-#-------------------------------------------
-#
-test_json_file() {
-        jq . $1 > /dev/null
-        if [ "$?" -gt 0 ] ; then
-                echo "ERROR: incorrect JSON FILE: $1"
-                exit 1
-        fi
+makeVariationsOfMergedPublicationAndCheckValidity() {
+  folder=$1
+  if [ $(find tmp/$folder/config -type f | wc -l) -gt 0 ]; then
+    jq --slurp -S '[.[][]]' $(find tmp/$folder/config -type f) >tmp/$folder/all.json
+    if [ "$?" -gt 0 ]; then
+      echo "ERROR: incorrect JSON FILE: $(find tmp/$folder/config -type f)"
+      exit 1
+    fi
+  else
+    echo "WARN: no config fount in tmp/$folder/config, falling back to empty list"
+    mkdir -p tmp/$folder
+    echo "[]" >tmp/$folder/all.json
+  fi
+
+  jq '.|=sort_by(.urlref)' tmp/$folder/all.json >tmp/$folder/publication.json
+  jq ' [.[] | .urlref ]' tmp/$folder/publication.json >tmp/$folder/uri.json
+  jq '[.[] | select( .disabled | not )]' tmp/$folder/publication.json | jq '.|=sort_by(.urlref)' >tmp/$folder/enabled_publication.json
+  jq ' [.[] | .urlref ]' tmp/$folder/enabled_publication.json >tmp/$folder/enabled_uri.json
 }
 
-
-# create a file with an empty list if the file does not exists or has an error 
-create_empty_list_json_file() {
-	if [ -f $1 ] ; then 
-		jq . $1 > /dev/null
-		if [ "$?" -gt 0 ] ; then
-			echo "ERROR: incorrect JSON FILE: $1"
-			exit 1
-		fi
-	else 
-	        echo "WARNING: generate file with empty list: $1"
-		echo "[]" > $1
-
-	fi
-}
-
-PUBLICATIONPOINTSDIRS=$(jq -r '.publicationpoints | @sh'  ${CONFIG_FOLDER}/config.json)
-PUBLICATIONPOINTSDIRS=`echo ${PUBLICATIONPOINTSDIRS} | sed -e "s/'//g"`
+PUBLICATIONPOINTSDIRS=$(jq -r '.publicationpoints | @sh' ${CONFIG_FOLDER}/config.json)
+PUBLICATIONPOINTSDIRS=$(echo ${PUBLICATIONPOINTSDIRS} | sed -e "s/'//g")
 
 # determine the last changed files
 # TOOLCHAIN_TOKEN is a PAT key configured in circleci as environment variable
@@ -66,57 +60,44 @@ if jq -e . $ROOT_DIR/commit.json; then
 
   listOfChanges=$(git diff --name-only --no-renames $COMMIT)
   echo "write change file"
-  echo $listOfChanges > changes.txt
+  echo $listOfChanges >changes.txt
 
-  for i in ${PUBLICATIONPOINTSDIRS} ; do
-         mkdir -p tmp/prev/config/$i
-         mkdir -p tmp/next/config/$i
+  for dir in ${PUBLICATIONPOINTSDIRS}; do
+    mkdir -p tmp/prev/config/$dir
+    mkdir -p tmp/next/config/$dir
   done
 
   GITROOT=${CONFIG_FOLDER#${CIRCLEWORKDIR}}
 
-  while read -r filename;  do
-#    if [[  $filename == "$CONFIG_FOLDER/$PUB_FILE" \
-#       || ($filename == $CONFIG_FOLDER/$PRODUCTION_FOLDER/*.$PUB_FILE && ($CIRCLE_BRANCH == "$TEST_BRANCH" || $CIRCLE_BRANCH == "$PRODUCTION_BRANCH")) \
-#       || ($filename == $CONFIG_FOLDER/$TEST_FOLDER/*.$PUB_FILE && $CIRCLE_BRANCH == "$TEST_BRANCH") \
-#       || ($filename == $CONFIG_FOLDER/$OTHER_FOLDER/*.$PUB_FILE && ($CIRCLE_BRANCH != "$TEST_BRANCH" && $CIRCLE_BRANCH != "$PRODUCTION_BRANCH")) \
-#       ]] ; then
+  # a development option to reduce the number of full rebuilds
+  TRIGGERALL=$(jq -r .toolchain.triggerall ${TOOLCHAINCONFIG})
+
+  while read -r filename; do
     filenameInSelection=false
-    for i in ${PUBLICATIONPOINTSDIRS} ; do
-           if [[ $filename =~ ${GITROOT}/$i/.*.${PUB_FILE} ]] ; then
-            filenameInSelection=true
-        fi
-           if [[ $filename =~ ${GITROOT}/$i/${PUB_FILE} ]] ; then
-            filenameInSelection=true
-        fi
+    for dir in ${PUBLICATIONPOINTSDIRS}; do
+      if [[ $filename == ${GITROOT}/$dir/*.${PUB_FILE} ]]; then
+        filenameInSelection=true
+      fi
+      if [[ $filename == ${GITROOT}/$dir/${PUB_FILE} ]]; then
+        filenameInSelection=true
+      fi
     done
 
-    # a development option to reduce the number of full rebuilds
-    TRIGGERALL=$(jq -r .toolchain.triggerall ${TOOLCHAINCONFIG})
-
-    if [[  $filename == "$CONFIG_FOLDER/$PUB_FILE" \
-       || $filenameInSelection == "true" \
-       ]] ; then
+    if [[ $filenameInSelection == "true" ]]; then
       echo "The file $filename is added for publicationchanges"
-      if git show $COMMIT:$filename &>/dev/null ; then
-        git show $COMMIT:$filename > tmp/prev/$filename
+      if git show $COMMIT:$filename &>/dev/null; then
+        git show $COMMIT:$filename >tmp/prev/$filename
       fi
-      if [[ -f $filename ]] ; then
+      if [[ -f $filename ]]; then
         cp $filename tmp/next/$filename
       fi
       changesRequireBuild=true
-    elif [[ $filename == $CONFIG_FOLDER/*/*.$PUB_FILE ]]; then
+    elif [[ $filename == $GITROOT/*/*.$PUB_FILE || $filename == $GITROOT/*.$PUB_FILE ]]; then
       echo "The file $filename is skipped as it is not part of the current environment"
-    elif [[ $filename == $GITROOT/*/*.$PUB_FILE ]]; then
-      echo "The file $filename is skipped as it is not part of the current environment"
-    elif [[ $filename == $GITROOT/*.$PUB_FILE ]]; then
-      echo "The file $filename is skipped as it is not part of the current environment"
-    elif [[ $filename == $GITROOT/*/$PUB_FILE ]]; then
-      echo "The file $filename is skipped as it is not part of the current environment"
-    elif [[ $filename == $GITROOT/$PUB_FILE ]]; then
+    elif [[ $filename == $GITROOT/*/$PUB_FILE || $filename == $GITROOT/$PUB_FILE ]]; then
       echo "The file $filename is skipped as it is not part of the current environment"
     elif [[ ${TRIGGERALL} == "false" ]]; then
-      echo "WARNING: a full rebuild is switched off. This is an development mode only choice" 
+      echo "WARNING: a full rebuild is switched off. This is an development mode only choice, changed file was: $filename"
     else
       echo "The file $filename is not a publication, everything will need to be processed"
       changesRequireBuild=true
@@ -131,24 +112,12 @@ fi
 #as there are only changes in the publication files, check if there are parts that are deleted, which means a full rebuild or are it only additions
 if [[ $changesRequireBuild == "true" && $onlyChangedPublicationFiles == "true" ]]; then
 
-	# ensure that all files at least exists with :q
-  create_empty_list_json_file tmp/next/publication.json
-  create_empty_list_json_file tmp/prev/publication.json
-  create_empty_list_json_file tmp/addedOrChanged.json
-  create_empty_list_json_file tmp/addedOrChangedUri.json
-  create_empty_list_json_file tmp/removedOrChanged.json
-  create_empty_list_json_file tmp/removedOrChangedUri.json
+  makeVariationsOfMergedPublicationAndCheckValidity next
+  makeVariationsOfMergedPublicationAndCheckValidity prev
 
-  jq --slurp -S '[.[][]]' $(find tmp/next/config -type f) | jq '[.[] | select( .disabled | not )]' | jq '.|=sort_by(.urlref)' > tmp/next/publication.json
-  jq --slurp -S '[.[][]]' $(find tmp/prev/config -type f) | jq '[.[] | select( .disabled | not )]' | jq '.|=sort_by(.urlref)' > tmp/prev/publication.json
-
-
-  jq -s '.[0] - .[1]' tmp/next/publication.json tmp/prev/publication.json > tmp/addedOrChanged.json
-  jq -s '.[1] - .[0]' tmp/next/publication.json tmp/prev/publication.json > tmp/removedOrChanged.json
-  jq ' [.[] | .urlref ]' tmp/addedOrChanged.json > tmp/addedOrChangedUri.json
-  jq ' [.[] | .urlref ]' tmp/removedOrChanged.json > tmp/removedOrChangedUri.json
-  jq -s '.[0]  - .[1] ' tmp/removedOrChangedUri.json tmp/addedOrChangedUri.json > tmp/removedUri.json
-  if jq -e '.[0]' tmp/removedUri.json > /dev/null 2>&1; then
+  #if you want to be sure disabled are removed when removed in next version change to tmp/prev/uri.json
+  jq -s '.[0] - .[1]' tmp/prev/enabled_uri.json tmp/next/uri.json >tmp/removedUri.json
+  if jq -e '.[0]' tmp/removedUri.json >/dev/null 2>&1; then
     onlyChangedPublications=false
   else
     onlyChangedPublications=true
@@ -159,38 +128,34 @@ fi
 
 if [[ $changesRequireBuild == "false" ]]; then
   #no changes for this environment
-  echo "true" > $ROOT_DIR/haschangedpublications.json
+  echo "true" >$ROOT_DIR/haschangedpublications.json
   cp ${PUB_CONFIG} $ROOT_DIR/publications.json.old
-  echo "[]" > ${PUB_CONFIG}
-  echo "[]" > $ROOT_DIR/changedpublications.json
+  echo "[]" >${PUB_CONFIG}
+  echo "[]" >$ROOT_DIR/changedpublications.json
   echo "kind of skip processing"
 elif [[ $onlyChangedPublications == "true" ]]; then
   #only changes in publication
-  echo "true" > $ROOT_DIR/haschangedpublications.json
+  echo "true" >$ROOT_DIR/haschangedpublications.json
   cp ${PUB_CONFIG} $ROOT_DIR/publications.json.old
+  jq -s '.[0] - .[1]' tmp/next/enabled_publication.json tmp/prev/enabled_publication.json >tmp/addedOrChanged.json
   cp tmp/addedOrChanged.json ${PUB_CONFIG}
   cp tmp/addedOrChanged.json $ROOT_DIR/changedpublications.json
   echo "process only added and updated publication points"
 else
   echo "include all selected publication points"
-  mkdir -p tmp/all
-  for i in ${PUBLICATIONPOINTSDIRS} ; do
-      mkdir -p tmp/all/$i
-	  echo "try to copy all files with extension ${PUB_FILE}"
-      cp ${CONFIG_FOLDER}/$i/*.${PUB_FILE}  tmp/all/$i
-	  echo "try to copy file ${PUB_FILE}"
-      cp ${CONFIG_FOLDER}/$i/${PUB_FILE}  tmp/all/$i
+  mkdir -p tmp/all/config
+  for dir in ${PUBLICATIONPOINTSDIRS}; do
+    mkdir -p tmp/all/config/$dir
+    echo "try to copy all files with extension ${PUB_FILE}"
+    cp ${CONFIG_FOLDER}/$dir/*.${PUB_FILE} tmp/all/config/$dir
+    echo "try to copy file ${PUB_FILE}"
+    cp ${CONFIG_FOLDER}/$dir/${PUB_FILE} tmp/all/config/$dir
   done
   echo "errors are normal if the files of the above form are not present"
-  jq --slurp -S '[.[][]]' $( find tmp/all -type f ) | jq '[.[] | select( .disabled | not )]' | jq '.|=sort_by(.urlref)' > $ROOT_DIR/allPublications.json
-  if [ "$?" -gt 0 ] ; then
-          echo "ERROR: one of the publication.json files contains a parse error"
-          exit 1
-  fi
-  test_json_file ${ROOT_DIR}/allPublications.json
-  echo "false" > $ROOT_DIR/haschangedpublications.json
+  makeVariationsOfMergedPublicationAndCheckValidity all
+  echo "false" >$ROOT_DIR/haschangedpublications.json
   cp ${PUB_CONFIG} $ROOT_DIR/publications.json.old
-  cp $ROOT_DIR/allPublications.json ${PUB_CONFIG}
-  cp $ROOT_DIR/allPublications.json $ROOT_DIR/changedpublications.json
+  cp tmp/all/enabled_publication.json ${PUB_CONFIG}
+  cp tmp/all/enabled_publication.json $ROOT_DIR/changedpublications.json
   echo "process all publication points"
 fi
