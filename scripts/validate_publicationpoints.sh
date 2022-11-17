7826f7ab00b33ec08b1dc2bfb47cebad29e5a046
Bert Van Nuffelen <bert.vannuffelen@gmail.com>
2022-11-17 20:15:25 +0000

    add check for existance of publication points

--- scripts/validate_publicationpoints.sh
+++ scripts/validate_publicationpoints.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# set -x
+
 # Arg1 = The configuration directory
 # Arg2 = The location where the generated repository is checked out
-# Arg3 = The CircleCI working directory
+# Arg3 = Stop when missing publication points are detected.
+# Arg4 = The CircleCI working directory
 
 CONFIGDIR=$1
 GENERATEDREPODIR=$2
+STOP=${3:-false}
+
 
 CIRCLEWORKDIR=$3 #set explicitly because CIRCLECI_WORKING_DIRECTORY is "~/project"
 
@@ -15,11 +20,17 @@ PUBLICATIONPOINTSDIRS=$(echo ${PUBLICATIONPOINTSDIRS} | sed -e "s/'//g")
 
 
 for dir in ${PUBLICATIONPOINTSDIRS}; do
-    echo $dir
-    PUBLICATIONPOINTSFILES=$(find  $dir -name *.publication.json )
+    echo "checking publication points in directory ${CONFIGDIR}/$dir"
+    echo ""
+    PUBLICATIONPOINTSFILES=$(find  ${CONFIGDIR}/$dir -name *.publication.json )
     for f in ${PUBLICATIONPOINTSFILES} ; do
-	    echo $f
-            check.sh $file ${GENERATEDREPODIR}
+            echo " + checking $f"
+            echo ""
+            ./scripts/check.sh $f ${GENERATEDREPODIR} ${STOP}
+            if  [ $? -ne 0 ] &&  ${STOP}   ; then
+                    echo "ERROR: missing publication points detected, please resolve them prior continuing"
+                    exit 1 ;
+            fi
     done
 done
 
