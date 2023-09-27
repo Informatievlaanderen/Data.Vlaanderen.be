365730d969c3d54703cdd8ecf454f16ac8a6bfc3
Bert Van Nuffelen <bert.vannuffelen@gmail.com>
2023-09-27 07:00:27 +0000

    add scripting for statistics building

---
+++ statistics/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+# deploy the template to the generated repository when creating the final commit
+# This deploy.sh will always be executed, but if no changes have happend
+# then this will not lead to a git commit
+#
+
+
